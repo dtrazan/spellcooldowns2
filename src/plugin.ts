dafc724097e4904a2a150ffc1ad530e92abc5289
dtrazan <tran1.DTT@gmail.com>
2026-02-13 21:13:11 -0800

    Removed counter action

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,15 +1,11 @@
 import streamDeck from "@elgato/streamdeck";
 
-import { IncrementCounter } from "./actions/increment-counter";
 import { ToggleItem } from "./actions/toggle-item";
 import { DisplayHaste } from "./actions/display-haste";
 import { GlobalSettingsManager } from "./global-settings";
 
 // We can enable "trace" logging so that all messages between the Stream Deck, and the plugin are recorded. When storing sensitive information
 streamDeck.logger.setLevel("trace");
-
-// Register the increment action.
-streamDeck.actions.registerAction(new IncrementCounter());
 
 // Register the toggle item action.
 streamDeck.actions.registerAction(new ToggleItem());
